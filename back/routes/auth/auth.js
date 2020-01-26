23215110e734ff129516c201035cf6347fc0f5d5
Inês Oliveira <inesfpoliveira@gmail.com>
2020-01-26 22:01:42 +0000

    Revising quest040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post('/signup', function (req, res, next) {
     const query = connection.query('INSERT INTO users (email, password, name, lastname) VALUES (?,?,?,?)', [req.body.email, req.body.password, req.body.name, req.body.lastname], function (err, results, fields) {
         if(error) res.status(500).end();
-        res.send('Welcome to this website!');
+        res.send('Welcome!');
         res.end();
     })
 })
