6504cd2915b37e41d94a2da0cad06f415993fa68
Inês Oliveira <inesfpoliveira@gmail.com>
2020-01-27 17:19:48 +0000

    quest060 is done and working

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import SignUp from './SignUp.js';
 import './App.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { Grid, Paper } from "@material-ui/core";
+
+const theme = createMuiTheme();
 
 function App() {
   return (
     <div className="App">
-      <SignUp/>
+      <MuiThemeProvider theme={theme}>
+        <Grid 
+          container
+          alignItems='center'
+          style={{ height: '100%' }}>
+          <Grid 
+            item xs={12}>
+            <Paper
+              elevation={4}
+              style={{ margin: 150 }}>
+              <Grid 
+                container
+                alignItems='center'
+                justify='center'
+                style={{ textAlign: 'center' }}>          
+                <img  src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png" alt="Homer eating donut" />
+                <Grid  
+                  item xs={12} sm={2}
+                  alignContent='center'>
+                  <SignUp />
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </MuiThemeProvider>
     </div>
   );
 }
--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Snackbar, TextField } from '@material-ui/core';
 
 class SignUp extends Component {
     constructor(props) {
@@ -10,10 +11,13 @@ class SignUp extends Component {
             name: "",
             lastname: "",
             flash: "",
+            isOpen: false,
         }
     }
 
-    updateEmailField = (e) => {
+    // In order for signup to work, we must yarn start on both front and back folders at the same time!
+
+    updateInfoField = (e) => {
         this.setState({
             [e.target.name]: e.target.value,
         });
@@ -36,45 +40,73 @@ class SignUp extends Component {
     e.preventDefault();
     }
 
+    handleOpen = () => {
+        this.setState((prevState) => ({
+          ...prevState,
+          isOpen: true
+        }))
+      }
+    
+      handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+        this.setState((prevState) => ({
+          ...prevState,
+          isOpen: false
+        }))
+      };
+
     render() {
+        const { name, lastname, email, password, repeat, flash, isOpen } = this.state;
         return (
             <div>
-                <h1>
-                    {JSON.stringify(this.state,1,1)}
-                </h1>
+                <h1>Sign up</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label>
-                            Email:
-                        </label>
-                        <input type="email" name="email" value={this.state.email} onChange={this.updateEmailField}></input>
-                    </div>
-                    <div>
-                        <label>
-                            Password:
-                        </label>
-                        <input type="text" name="password" value={this.state.password} onChange={this.updateEmailField}></input>
-                    </div>
-                    <div>
-                        <label>
-                            Repeat Password:
-                        </label>
-                        <input type="text" name="repeat" value={this.state.repeat} onChange={this.updateEmailField}></input>
-                    </div>
-                    <div>
-                        <label>
-                            Name:
-                        </label>
-                        <input type="text" name="name" value={this.state.name} onChange={this.updateEmailField} ></input>
-                    </div>
-                    <div>
-                        <label>
-                            Last Name:
-                        </label>
-                        <input type="text" name="lastname" value={this.state.lastname} onChange={this.updateEmailField} ></input>
-                    </div>
-                    <input type="submit" value="Submit" />
+                    <TextField
+                        label="Email"
+                        type="email"
+                        name="email"
+                        onChange={this.updateInfoField}
+                        value={email}/>
+                    <TextField
+                        label="Password"
+                        type="password"
+                        name="password"
+                        onChange={this.updateInfoField}
+                        value={password}/>    
+                    <TextField 
+                        label="Repeat password"
+                        type="password"
+                        name="repeat"
+                        onChange={this.updateInfoField}
+                        value={repeat}/>
+                    <TextField
+                        label="Name"
+                        type="name"
+                        name="name"
+                        onChange={this.updateInfoField}
+                        value={name}/>
+                    <TextField
+                        label="Lastname"
+                        type="lastname"
+                        name="lastname"
+                        onChange={this.updateInfoField}
+                        value={lastname}/>
+                    <Button 
+                        type="submit"
+                        variant="outlined" 
+                        color="primary"
+                        size="large"
+                        onClick={this.handleOpen}>Submit
+                    </Button>
                 </form>
+                <Snackbar
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                    open={isOpen}
+                    autoHideDuration={4000}
+                    onClose={this.handleClose}
+                    message={flash}/>
             </div>
         );
     }
